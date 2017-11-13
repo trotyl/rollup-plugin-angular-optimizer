657c3e3136adde5526418019d050fe9d2adf237f
Trotyl <trotyl@qq.com>
2017-11-13 13:50:23 +0800

    enable sourcemap

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 "use strict";
-exports.__esModule = true;
-var build_optimizer_1 = require("@angular-devkit/build-optimizer");
+Object.defineProperty(exports, "__esModule", { value: true });
+const build_optimizer_1 = require("@angular-devkit/build-optimizer");
 function optimizer() {
     return {
         name: 'angular-optimizer',
-        transform: function (content) { return build_optimizer_1.buildOptimizer({ content: content }).content; }
+        transform: (content, id) => {
+            const { content: code, sourceMap: map } = build_optimizer_1.buildOptimizer({ content, inputFilePath: id, emitSourceMap: true });
+            if (!code) { return null }
+            if (!map) { return code }
+            return { code, map };
+        },
     };
 }
-exports["default"] = optimizer;
+exports.default = optimizer;
--- index.ts
+++ index.ts
@@ -3,7 +3,12 @@ import { buildOptimizer } from '@angular-devkit/build-optimizer'
 export default function optimizer() {
   return {
     name: 'angular-optimizer',
-    transform: (content) => buildOptimizer({ content }).content,
+    transform: (content, id) => { 
+      const { content: code, sourceMap: map } = buildOptimizer({ content, inputFilePath: id, emitSourceMap: true })
+      if (!code) { return null }
+      if (!map) { return code }
+      return { code, map }
+    },
   }
 }
 
